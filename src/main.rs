8482c8cd3e51cab6cc2d0d493bb2dfc6d6a58b73
David Bradford <dbradf@gmail.com>
2023-02-20 18:55:11 -0500

    better reporting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::{collections::HashMap, fs};
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+    fs,
+    time::Instant,
+};
 
 use ignore::{DirEntry, Walk};
 use itertools::Itertools;
 use sha2::{Digest, Sha256};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Location {
     file: usize,
     line: usize,
@@ -13,12 +18,19 @@ struct Location {
 fn main() {
     let mut files = vec![];
     let mut lines: HashMap<String, Vec<Location>> = HashMap::new();
+    let start = Instant::now();
     for result in Walk::new(".") {
         match result {
             Ok(entry) => hash_file(entry, &mut files, &mut lines),
             Err(err) => eprintln!("ERROR: {}", err),
         }
     }
+
+    let end = Instant::now();
+    println!(
+        "build map time: {}ms",
+        end.duration_since(start).as_millis()
+    );
 
     let dup_map = lines.iter().filter(|&(_, v)| v.len() > 1);
     let mut file_map: HashMap<String, HashMap<usize, String>> = HashMap::new();
@@ -36,40 +48,94 @@ fn main() {
         }
     }
 
-    for f in &files {
-        if let Some(locations) = file_map.get(f) {
-            let mut file_printed = false;
-            for (line, hash) in locations {
-                // if locations.get(&(line + 1)).is_some() && locations.get(&(line + 2)).is_some() {
-                if true {
-                    let mut i = line + 2;
-                    while locations.get(&i).is_some() {
-                        i += 1;
-                    }
-                    if !file_printed {
-                        println!("{}:\n", f);
-                        file_printed = true;
-                    }
-                    println!("  {}-{}: {}", line, i, hash);
-                    if let Some(matches) = lines.get(hash) {
-                        for x in matches {
-                            let filename = &files[x.file];
-                            println!(" - {}: {}", filename, x.line);
+    files.iter().enumerate().for_each(|(i, f)| {
+        let mut matches = matches_for_file(f, i, &file_map, &lines);
+        if !matches.is_empty() {
+            println!("\n{}:\n", f);
+
+            matches.sort_by(|a, b| a.start.partial_cmp(&b.start).unwrap());
+            for m in matches {
+                let filename = &files[m.file];
+                println!(
+                    "- {} - {} :{}: {} - {}",
+                    m.start, m.end, filename, m.remote_start, m.remote_end
+                );
+            }
+        }
+    });
+}
+
+#[derive(Debug)]
+struct CopyPasteMatch {
+    start: usize,
+    end: usize,
+    file: usize,
+    remote_start: usize,
+    remote_end: usize,
+}
+
+impl CopyPasteMatch {
+    fn is_overlapping(&self, start: usize, end: usize, file: usize) -> bool {
+        if file != self.file {
+            return false;
+        }
+        if start >= self.start && start <= self.end {
+            return true;
+        }
+
+        end >= self.start && end <= self.end
+    }
+
+    fn is_duplicate(&self, file: usize) -> bool {
+        file == self.file && self.start == self.remote_start && self.end == self.remote_end
+    }
+
+    fn expand(&mut self, start: usize, end: usize, remote_start: usize, remote_end: usize) {
+        self.start = min(self.start, start);
+        self.end = max(self.end, end);
+        self.remote_start = min(self.remote_start, remote_start);
+        self.remote_end = max(self.remote_end, remote_end);
+    }
+}
+
+fn matches_for_file(
+    f: &str,
+    file_index: usize,
+    file_map: &HashMap<String, HashMap<usize, String>>,
+    lines: &HashMap<String, Vec<Location>>,
+) -> Vec<CopyPasteMatch> {
+    let mut match_list: Vec<CopyPasteMatch> = vec![];
+    if let Some(locations) = file_map.get(f) {
+        for (line, hash) in locations {
+            if let Some(matches) = lines.get(hash) {
+                for x in matches {
+                    let start = *line;
+                    let end = start + 4;
+                    let mut found = false;
+                    for m in &mut match_list {
+                        if m.is_overlapping(start, end, x.file) {
+                            m.expand(start, end, x.line, x.line + 4);
+                            found = true;
+                            break;
                         }
+                    }
+                    if !found {
+                        match_list.push(CopyPasteMatch {
+                            start,
+                            end,
+                            file: x.file,
+                            remote_start: x.line,
+                            remote_end: x.line + 4,
+                        })
                     }
                 }
             }
         }
     }
-
-    // for (_, v) in lines.iter().filter(|&(_, v)| v.len() > 1) {
-    //     println!("Dup");
-    //     for l in v {
-    //         let filename = &files[l.file];
-    //         println!("{}: line {}", filename, l.line);
-    //     }
-    //     println!();
-    // }
+    match_list
+        .into_iter()
+        .filter(|m| !m.is_duplicate(file_index))
+        .collect()
 }
 
 fn hash_file(entry: DirEntry, files: &mut Vec<String>, lines: &mut HashMap<String, Vec<Location>>) {
@@ -89,7 +155,7 @@ fn hash_file(entry: DirEntry, files: &mut Vec<String>, lines: &mut HashMap<Strin
         .tuple_windows::<(_, _, _, _)>()
         .for_each(|((i, l0), (_, l1), (_, l2), (_, l3))| {
             let line = [l0, l1, l2, l3].map(|s| s.trim()).join("\n");
-            if line.len() < 10 {
+            if line.len() < 15 {
                 return;
             }
             let h = hash_line(&line);
